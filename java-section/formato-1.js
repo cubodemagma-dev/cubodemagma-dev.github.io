8bca485caf8bfd43484d81f0ad0eb2651efca009
cubodemagma-dev <cubodemagma.dev@gmail.com>
2025-11-22 04:19:58 -0600

    Update formato-1.js

--- java-section/formato-1.js
+++ java-section/formato-1.js
@@ -1,12 +1,12 @@
 function abrirSeccion(nombreSeccion) {
   var todasLasSecciones = document.getElementsByClassName("seccion");
 
-  for (var i = 0; i < todasLasSecciones.lenght; i++) {
+  for (var i = 0; i < todasLasSecciones.length; i++) {
     todasLasSecciones[i].classList.remove("activo");
   }
 
   var seccionAMostrar = document.getElementById(nombreSeccion);
   if (seccionAMostrar) {
-    SeccionAMostrar.classList.add("activo");
+    seccionAMostrar.classList.add("activo");
   }
 }
