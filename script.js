c0f701630d0163d47fb04846d7f331a905bf138a
cubodemagma-dev <cubodemagma.dev@gmail.com>
2026-02-23 16:05:00 -0600

    Fix logo image source path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function loadLogo() {
     const logoContainer = document.getElementById('logoContainer');
     const img = document.createElement('img');
-    img.src = 'imagenes/GRAF01.png';
+    img.src = './imagenes/GRAF01.png';
     img.alt = 'Logo del estudio AeloBOX';
     
     img.onerror = function() {
